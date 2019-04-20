308bf7c4d940909e9e1130d0a26d8771253e5e5e
alexander <alexander@alexander.alexander>
2019-04-20 10:48:48 +0300

    Posting to group wall

--- main.py
+++ main.py
@@ -1,9 +1,44 @@
+import os
+import sys
 import vk_api
 import requests
+import configparser
+
+
+def config():
+    cfg = configparser.ConfigParser()
+    cfg.read('usr.ini')
+    section = cfg['LOGGING']
+
+    return {
+        'login': section['login'],
+        'id_': section['id_']
+    }
+
+
+def vk_auth(cfg):
+    session = vk_api.VkApi(
+        login=cfg['login'],
+        token=cfg['token']
+    )
+
+    try:
+        session.auth(reauth=True, token_only=True)
+    except vk_api.AuthError as ex:
+        print(ex)
+        sys.exit()
+
+    return session.get_api()
 
 
 def main():
-    pass
+    cfg = config()
+    session = vk_auth(cfg)
+    session.wall.post(
+        message='Всё в порядке! Валентин сегодня не умер!',
+        owner_id=-cfg['group_id'],
+        from_group=cfg['from_group']
+    )
 
 
 if __name__ == '__main__':
