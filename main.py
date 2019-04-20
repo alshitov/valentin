7aac31a27144474cf9f86b6ad4b17e7e94a2e93c
alexander <alexander@alexander.alexander>
2019-04-20 11:43:53 +0300

    final version

--- main.py
+++ main.py
@@ -2,26 +2,18 @@ import os
 import sys
 import vk_api
 import random
-import configparser
+import json
 
 
-def config():
-    cfg = configparser.ConfigParser()
-    cfg.read('usr.ini')
-    section = cfg['LOGGING']
-
-    return {
-        'login': section['login'],
-        'token': section['token'],
-        'owner_id': section['owner_id'],
-        'from_group': section['from_group']
-    }
+def read_content():
+    with open('content.json', mode='r') as f:
+        return json.load(f)
 
 
-def vk_auth(cfg):
+def vk_auth(content):
     session = vk_api.VkApi(
-        login=cfg['login'],
-        token=cfg['token']
+        login=content['logging']['login'],
+        token=content['logging']['token']
     )
 
     try:
@@ -33,23 +25,39 @@ def vk_auth(cfg):
     return session.get_api()
 
 
+def rand_text(content):
+    greets = content['greet']
+    names = content['name']
+    rand_greet = greets[random.randint(a=0, b=len(greets))]
+    rand_name = names[random.randint(a=0, b=len(names))]
+
+    return rand_greet, rand_name
+
+
 def attach(upload):
     randint = random.randint(a=1, b=10)
     dir_ = os.path.dirname(os.path.realpath(__file__))
     photo = '{0}/img/{1}.jpg'.format(dir_, randint)
-    photo_list = upload.photo_wall(photo)
+    photo_list = upload.photo_wall(
+        photos=photo
+    )
+
     return ','.join('photo{owner_id}_{id}'.format(**item) for item in photo_list)
 
 
 def main():
-    cfg = config()
-    session = vk_auth(cfg)
+    content = read_content()
+    session = vk_auth(content)
     upload = vk_api.VkUpload(session)
     attachment = attach(upload)
+    rand_text_ = rand_text(content)
+
     session.wall.post(
-        message='Всё в порядке! Валентин сегодня не умер!',
-        owner_id=cfg['owner_id'],
-        from_group=cfg['from_group'],
+        message='{0}! {1} сегодня не умер!'.format(
+            rand_text_[0], rand_text_[1]
+        ),
+        owner_id=content['logging']['owner_id'],
+        from_group=content['logging']['from_group'],
         attachment=attachment
     )
 
